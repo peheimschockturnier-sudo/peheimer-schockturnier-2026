8f868e24954b31fbc07cf5262d9f65eaa3ad1b54
peheimschockturnier-sudo <peheimschockturnier@gmail.com>
2026-07-29 17:04:46 +0200

    Fix formatting of WEBAPP_URL assignment

--- js/config.js
+++ js/config.js
@@ -1,2 +1,1 @@
-const WEBAPP_URL =
-  "https://script.google.com/macros/s/AKfycbyC8HVOroZPdbWHKviQmz-lieJTwoJYAyyUZWCq_Ew4qC72XInFMHO1SQBpHdl1agMqrA/exec";
+const WEBAPP_URL ="https://script.google.com/macros/s/AKfycbyC8HVOroZPdbWHKviQmz-lieJTwoJYAyyUZWCq_Ew4qC72XInFMHO1SQBpHdl1agMqrA/exec";
