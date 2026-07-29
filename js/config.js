1dc52dad126bd918a2df34ad67e9de39d1d73fff
peheimschockturnier-sudo <peheimschockturnier@gmail.com>
2026-07-29 17:19:01 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-const WEBAPP_URL ="https://script.google.com/macros/s/AKfycbyC8HVOroZPdbWHKviQmz-lieJTwoJYAyyUZWCq_Ew4qC72XInFMHO1SQBpHdl1agMqrA/exec";
+window.WEBAPP_URL ="https://script.google.com/macros/s/AKfycbyC8HVOroZPdbWHKviQmz-lieJTwoJYAyyUZWCq_Ew4qC72XInFMHO1SQBpHdl1agMqrA/exec";
