d1a4c8aa7ef0d906dc6e5dab1f9bc4a81b74fb5a
peheimschockturnier-sudo <peheimschockturnier@gmail.com>
2026-07-29 16:10:07 +0200

    Set TESTMODUS to true

--- js/freigabe.js
+++ js/freigabe.js
@@ -1,4 +1,4 @@
-const TESTMODUS = false;
+const TESTMODUS = true;
 const FREIGABE_DATUM = new Date("2026-09-01T00:00:00");
 
 const WEBAPP_URL="https://script.google.com/macros/s/AKfycbyC8HVOroZPdbWHKviQmz-lieJTwoJYAyyUZWCq_Ew4qC72XInFMHO1SQBpHdl1agMqrA/exec";
